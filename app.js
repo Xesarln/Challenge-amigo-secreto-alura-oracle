ec56460ead4bfae0cea7f727aac556f94f105a54
Xesarln <cjdcomputadoras@gmail.com>
2025-09-19 03:08:59 -0500

    Add files via upload
    challenge del amigo secreto, propuesto por alura para culminar logica de programacion

---
+++ app.js
@@ -0,0 +1,5 @@
+let amigos = [];
+//Variables con objetos HTML 
+let listaHTML = document.querySelector('#listaAmigos');
+let ingresarAmigos = document.querySelector('#amigo');
+let resultado = document.querySelector('#resultado');
