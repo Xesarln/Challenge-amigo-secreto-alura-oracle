88a2ecf6fc955c593ab377c770b53ae276bddc89
Xesarln <cjdcomputadoras@gmail.com>
2025-09-19 03:15:55 -0500

    agregando fn

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ let amigos = [];
 let listaHTML = document.querySelector('#listaAmigos');
 let ingresarAmigos = document.querySelector('#amigo');
 let resultado = document.querySelector('#resultado');
+//agregar amigos
+function agregarAmigo() {
+    let nombre = ingresarAmigos.value.trim(); //trim() lo usamos para borrar espacios en blanco al comienzo y al final
+
+    if(nombre === ''){
+        alert('Por favor, ingresa un nombre de un amigo');
+        return;
+    }
+
+    if(amigoRepetido(nombre) === 'agregar'){
+        amigos.push(nombre); //agregar nombre
+        ingresarAmigos.value = ""; //Vaciar input
+        listarAmigos(); //Actualizar
+    }
+
+    return;
+}
